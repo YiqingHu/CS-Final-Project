003174467b5bef44617891a50667fae3661f348b
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-01-24 21:48:35 -0500

    complete the class point
    class point
    added constructor and toString method

--- Point.java
+++ Point.java
@@ -1,11 +1,13 @@
 public class Point {
-  private int x;
-  private int y;
+  private int _x;
+  private int _y;
   
   public Point(int x, int y){
-  
+  	_x = x;
+  	_y = y;
   }
   
   public String toString() {
+  	return "(" + _x + "," + _y + ")";
   }
 }
