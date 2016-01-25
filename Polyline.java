156facfa6a7e81af24d849362f006844dcb65855
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-01-24 22:19:58 -0500

    update
    finish

--- Polyline.java
+++ Polyline.java
@@ -2,27 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Polyline{
-	private List<Point> points = new ArrayList<Point> ();
+ private List<Point> points = new ArrayList<Point> ();
 
-	public Polyline(){
+ public Polyline(){
+ }
 
-	}
+ public void appendPoint(int x, int y){
+  Point newPoint = new Point(x,y);
+  points.add(newPoint);
+ }
 
-	public void appendPoint(int x, int y){
-		Point newPoint = new Point(x,y);
-		points.add(newPoint);
-	}
+ public void appendPoint(Point point){
+  points.add(point);
+ }
 
-	public void appendPoint(Point point){
-		points.add(point);
-	}
-
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		for (Point aPoint : points){
-			sb.append(aPoint.toString());
-		}
-		return sb.toString();
-	}
+ public String toString(){
+  StringBuilder sb = new StringBuilder();
+  for (Point aPoint : points){
+   sb.append(aPoint.toString());
+  }
+  return sb.toString();
+ }
 
 }
--- TestPolyline.java
+++ TestPolyline.java
@@ -1,10 +1,10 @@
 public class TestPolyline{
-	public static void main(String[] args){
-		Polyline l1 - new Polyline();
-		System.out.println(l1);
-		l1.appendPoint(new Point(1,1));
-		ll.appendPoint(new Point(2,2));
-		l1.appendPoint(new Point(3,3));
-		System.out.println(l1);
-	}
+ public static void main(String[] args){
+  Polyline l1 = new Polyline();
+  System.out.println(l1);
+  l1.appendPoint(new Point(1,1));
+  l1.appendPoint(new Point(2,2));
+  l1.appendPoint(new Point(3,3));
+  System.out.println(l1);
+ }
 }
