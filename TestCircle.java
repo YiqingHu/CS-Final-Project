49ae27a5604b5efe6839fa8bce181746933ae1cf
KusuDesu <wchan6@stuy.edu>
2016-01-25 21:59:38 -0500

    Update TestCircle.java

--- TestCircle.java
+++ TestCircle.java
@@ -3,8 +3,11 @@ public class TestCircle.java{
   public static void main(String [] args){
   
     Circle x = new Circle(1.0);
+    double radius = x.getRadius();
+    System.out.println("The radius is: " + radius);
     double perimeter = x.getPerimeter();
-    System.out.println(perimeter);
-    
+    System.out.println("The perimeter is: " + perimeter);
+    double area = x.getArea();
+    System.out.println("The area is: " + area);
   }
 }
