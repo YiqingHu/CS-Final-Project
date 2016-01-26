2bc92704e107957857ecae50701d8849c8c745f6
KusuDesu <wchan6@stuy.edu>
2016-01-25 22:56:03 -0500

    Complete

--- ResizableCircle.java
+++ ResizableCircle.java
@@ -1,9 +1,15 @@
 public class ResizableCircle extends Circle implements Resizable{
 
+  
+  //time: O(1)
+  //preconditions: r is a double
+  //postconditions: _radius is assigned the value of r
   public ResizableCircle(double radius){
     super(radius);
   }
-  
+  //time: O(1)
+  //preconditions: getRadius returns a double, percent is an int
+  //postconditions: returns double
   public double resize(int percent){
     return (percent * 0.01 * getRadius());
   }
