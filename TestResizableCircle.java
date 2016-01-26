936c4fbc283b337da5f001a0502e21acfcbf9774
KusuDesu <wchan6@stuy.edu>
2016-01-25 22:33:33 -0500

    Update TestResizableCircle.java

--- TestResizableCircle.java
+++ TestResizableCircle.java
@@ -2,7 +2,7 @@ public class TestResizableCircle{
 
   public static void main(String [] args){
   
-    Circle x = new Circle(1.0);
+    ResizableCircle x = new ResizableCircle(1.0);
     double radius = x.getRadius();
     System.out.println("The original radius is: " + radius);
     double newRadius = x.resize(50);
