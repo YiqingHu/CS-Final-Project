d8e5bcee9da6ccd854ff3b01fa62ecb572e194e1
KusuDesu <wchan6@stuy.edu>
2016-01-25 22:56:26 -0500

    Complete

--- TestCircle.java
+++ TestCircle.java
@@ -1,8 +1,9 @@
-public class TestCircle.java{
+public class TestCircle{
 
   public static void main(String [] args){
   
     Circle x = new Circle(1.0);
+    System.out.println(x);
     double radius = x.getRadius();
     System.out.println("The radius is: " + radius);
     double perimeter = x.getPerimeter();
