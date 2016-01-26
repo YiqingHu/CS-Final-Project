10f0a1c83ace3cb9e936eb336d0b4b09c32fe0e9
KusuDesu <wchan6@stuy.edu>
2016-01-25 21:53:23 -0500

    Create TestCircle.java

---
+++ TestCircle.java
@@ -0,0 +1,10 @@
+public class TestCircle.java{
+
+  public static void main(String [] args){
+  
+    Circle x = new Circle(1.0);
+    double perimeter = x.getPerimeter();
+    System.out.println(perimeter);
+    
+  }
+}
