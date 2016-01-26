91b3fe614d1723a4a23d26396153c0645db6aa3f
KusuDesu <wchan6@stuy.edu>
2016-01-25 22:52:16 -0500

    Complete

--- Circle.java
+++ Circle.java
@@ -2,18 +2,30 @@ public class Circle implements GeometricObject{
 
   private double _radius;
   
+  //time: O(1)
+  //preconditions: r is a double
+  //postconditions: _radius is assigned the value of r
   public Circle(double r){
     _radius = r;
   }
   
+  //time: O(1)
+  //preconditions: _radius is a double
+  //postconditions: return double _radius
   public double getRadius(){
     return _radius;
   }
   
+  //time: O(1)
+  //preconditions: getRadius returns a double 
+  //postconditions: returns double
   public double getPerimeter(){
     return (getRadius() * 2.0 * Math.PI);
   }
   
+  //time: O(1)
+  //preconditions: getRadius returns a double
+  //postconditions: returns a double
   public double getArea(){
     return (getRadius() * getRadius() * Math.PI);
   }
